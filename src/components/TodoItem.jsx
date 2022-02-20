3d9007d389b7e15d58cfffe1d67030dafa7d281a
197491 <197491@TCR-00349>
2022-02-20 03:25:36 -0300

    checkbox na cor azul

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,7 +12,7 @@ export default function Todoitem({ todo, deleteTodo }) {
                 style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginTop: "10px", padding: "10px", fontWeight:"bold" }}>
 
                 <div>
-                    <span> <Checkbox /> </span>
+                    <span> <Checkbox color="primary" /> </span>
                     <span style={{ lineBreak: "anywhere", marginRight: "10px" }}> {todo.nome} </span>
                 </div>
 
